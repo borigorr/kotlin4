e0db910daeaa0ecac99dddc7b5f7ac90dc08d2d7
Borigor <borigorr@gmail.com>
2023-04-19 17:19:34 +0300

    init

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+
+fun main() {
+    println(calcCommission(100.0, cardType = "Visa"))
+}
+
+fun calcCommission(
+    amount: Double,
+    amountCurrentMonth: Double = 0.0,
+    cardType: String = "VKPay"
+): Double = when {
+    cardType == "VKPay" && (amount > 15_000 || amountCurrentMonth > 40_000) -> throw LimitException()
+    cardType != "VKPay" && (amountCurrentMonth > 600_000 || amount > 150_000) -> throw LimitException()
+    (cardType == "MasterCard" || cardType == "Maestro") && amountCurrentMonth > 75_000 -> amount * 0.006 + 20
+    cardType == "Visa" || cardType == "Мир" -> {
+        val commission: Double = amount * 0.0075;if (commission < 35) 35.0 else commission
+    }
+    else -> 0.0
+}
+
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,72 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainKtTest {
+
+    @Test
+    fun calcCommissionDefaultParams() {
+        val amount = 100.0
+        val result = calcCommission(amount)
+        assertEquals(0.0, result)
+    }
+
+    @Test(expected = LimitException::class)
+    fun calcCommissionVkPayMaxAmount() {
+        val amount = 15_001.0
+        val amountCurrentMonth = 500.0
+        val cardType = "VKPay"
+        calcCommission(amount, amountCurrentMonth, cardType)
+    }
+
+    @Test(expected = LimitException::class)
+    fun calcCommissionVkPayMaxAmountCurrentMonth() {
+        val amount = 1_001.0
+        val amountCurrentMonth = 40_001.0
+        val cardType = "VKPay"
+        calcCommission(amount, amountCurrentMonth, cardType)
+    }
+
+    @Test(expected = LimitException::class)
+    fun calcCommissionCardMaxAmountCurrentMonth() {
+        val amount = 1_001.0
+        val amountCurrentMonth = 600_001.0
+        val cardType = "MasterCard"
+        calcCommission(amount, amountCurrentMonth, cardType)
+    }
+
+    @Test(expected = LimitException::class)
+    fun calcCommissionCardMaxAmount() {
+        val amount = 150_001.0
+        val amountCurrentMonth = 60_001.0
+        val cardType = "MasterCard"
+        calcCommission(amount, amountCurrentMonth, cardType)
+    }
+
+    @Test
+    fun calcCommissionMasterCard() {
+        val amount = 100.0
+        val amountCurrentMonth = 75_001.0
+        val cardType = "MasterCard"
+        val result = calcCommission(amount, amountCurrentMonth, cardType)
+        assertEquals(20.6, result)
+    }
+
+    @Test
+    fun calcCommissionMaestroCard() {
+        val amount = 100.0
+        val amountCurrentMonth = 75_001.0
+        val cardType = "Maestro"
+        val result = calcCommission(amount, amountCurrentMonth, cardType)
+        assertEquals(20.6, result)
+    }
+
+    @Test
+    fun calcCommissionVisaMin() {
+        val amount = 100.0
+        val amountCurrentMonth = 50.0
+        val cardType = "Visa"
+        val result = calcCommission(amount, amountCurrentMonth, cardType)
+        assertEquals(35.0, result)
+    }
+}
